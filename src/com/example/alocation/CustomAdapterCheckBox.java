e6e2757e982ea4741ac989736146a5e171647f95
kgaurav0212 <kgaurav0212@gmail.com>
2014-08-28 23:06:27 +0530

    a

---
+++ src/com/example/alocation/CustomAdapterCheckBox.java
@@ -0,0 +1,82 @@
+package com.example.alocation;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+
+
+
+public class CustomAdapterCheckBox extends BaseAdapter {
+
+
+	private LayoutInflater mInflater;
+	ArrayList<Integer> selectedIds = new ArrayList<Integer>();
+	ArrayList<LocData> locationData;
+
+	public CustomAdapterCheckBox(Context c, ArrayList<LocData> locationData1) {
+		mInflater = (LayoutInflater)  c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		locationData = locationData1;
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return locationData.size();
+	}
+
+	@Override
+	public Object getItem(int arg0) {
+		// TODO Auto-generated method stub
+		return locationData.get(arg0);
+	}
+
+	@Override
+	public long getItemId(int arg0) {
+		// TODO Auto-generated method stub
+		return arg0;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		if (convertView == null) {
+			convertView = mInflater.inflate(R.layout.check_box_view, null);
+		}
+
+		CheckBox check1 = (CheckBox) convertView.findViewById(R.id.checkBox1);
+
+		check1.setText(locationData.get(position).getName() );
+		check1.setId(locationData.get(position).getId());
+
+
+		final int index = position;
+		check1.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if (isChecked) {
+					selectedIds.add( index );
+				}else{
+					selectedIds.remove(  selectedIds.lastIndexOf( index ) );
+				}
+
+			}
+
+		});
+
+		return convertView;
+	}
+
+	public ArrayList<Integer> getSelectedIds(){
+		return selectedIds;
+	}
+
+
+}
---
+++ src/com/example/alocation/LocData.java
@@ -0,0 +1,44 @@
+package com.example.alocation;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class LocData {
+
+		double lat;
+		double lng;
+		int id;
+		String name;
+		
+		public void setLatLng(double lat, double lng) {
+			this.lat = lat;
+			this.lng = lng;
+		}
+		
+		public double getLat() {
+			return lat;
+		}
+		
+		public double getLng() {
+			return lng;
+		}
+		
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+		
+		public LatLng getPosition(){
+			return new LatLng(lat, lng);
+		}
+		
+		
+}
